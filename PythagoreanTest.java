cbf1222ac280ab4dc8ba61b48e6a7faf452c512c
jguadamuz <44035647+jguadamuz@users.noreply.github.com>
2019-06-15 13:28:46 -0500

    Assignment submitted

---
+++ PythagoreanTest.java
@@ -0,0 +1,13 @@
+/**
+ * PythagoreanTest
+ */
+public class PythagoreanTest {
+
+    public static void main(String[] args) {
+        Pythagorean py = new Pythagorean();
+
+        System.out.println(py.calculateHypotenuse(3, 4));
+        System.out.println(py.calculateHypotenuse(6, 8));
+        System.out.println(py.calculateHypotenuse(9, 12));
+    }
+}
